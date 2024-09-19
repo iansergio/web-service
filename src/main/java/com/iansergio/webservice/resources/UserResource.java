2ef49ff3d73e8aaa2fa1ef6fde879ac85cec42a1
ian <iansergiohelf@gmail.com>
2024-09-19 00:06:46 -0300

    :sparkles: feat: Adicionando camada de serviço

--- src/main/java/com/iansergio/webservice/resources/UserResource.java
+++ src/main/java/com/iansergio/webservice/resources/UserResource.java
@@ -1,18 +1,32 @@
 package com.iansergio.webservice.resources;
 
 import com.iansergio.webservice.entities.User;
+import com.iansergio.webservice.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/user")
-public class UserResource {
+@RequestMapping("/users")
+public class UserResource   {
+
+    @Autowired
+    private UserService service;
 
     @GetMapping
-    public ResponseEntity<User> getAll(){
-        User u = new User(1L, "joao", "joao@gmail.com", "000000000", "123");
-        return ResponseEntity.ok().body(u);
+    public ResponseEntity<List<User>> findAll(){
+        List<User> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id){
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
     }
 }
